5c43e9796934f7c7c37019a6410d26e659c2552f
shubham naruka <snaruka@localhost.localdomain>
2024-12-09 08:01:20 -0700

    Alien file

--- src/Alien.cpp
+++ src/Alien.cpp
@@ -8,25 +8,28 @@
 /**
  * Alien is a ZeroMQ Sub socket.
  */
-Alien::Alien() { 
-   mCtx = zctx_new();
-   CHECK(mCtx!=nullptr);
-   mBody = zsocket_new(mCtx, ZMQ_SUB);
-   CHECK(mBody!=nullptr);
+Alien::Alien()
+{
+   mCtx = zmq_ctx_new();
+   CHECK(mCtx != nullptr);
+   mBody = zmq_socket(mCtx, ZMQ_SUB);
+   CHECK(mBody != nullptr);
 }
 
 /**
  * Setup the location to receive messages.
  * @param location
  */
-void Alien::PrepareToBeShot(const std::string& location) {
-   //Subscribe to everything
+void Alien::PrepareToBeShot(const std::string &location)
+{
+   // Subscribe to everything
    char dummy = '\0';
    zsocket_set_subscribe(mBody, &dummy);
    zsocket_set_rcvhwm(mBody, 32 * 1024);
    zsocket_set_sndhwm(mBody, 32 * 1024);
    int rc = zsocket_connect(mBody, location.c_str());
-   if (rc == -1) {
+   if (rc == -1)
+   {
       LOG(WARNING) << "location: " << location;
       LOG(WARNING) << "connect socket rc == " << rc;
       throw std::string("Failed to connect to socket");
@@ -35,15 +38,18 @@ void Alien::PrepareToBeShot(const std::string& location) {
 
 /**
  * Blocking call that returns when the alien has been shot.
- * @return 
+ * @return
  */
-std::vector<std::string> Alien::GetShot() {
+std::vector<std::string> Alien::GetShot()
+{
    std::vector<std::string> bullets;
-   while (!zctx_interrupted && bullets.empty()) {
+   while (!zctx_interrupted && bullets.empty())
+   {
       GetShot(1000, bullets);
       boost::this_thread::interruption_point();
    }
-   if (zctx_interrupted) {
+   if (zctx_interrupted)
+   {
       LOG(INFO) << "Caught Interrupt Signal";
    }
    return bullets;
@@ -51,49 +57,60 @@ std::vector<std::string> Alien::GetShot() {
 
 /**
  * Blocking call that returns when the alien has been shot.
- * @return 
+ * @return
  */
-void Alien::GetShot(const unsigned int timeout, std::vector<std::string>& bullets) {
+void Alien::GetShot(const unsigned int timeout, std::vector<std::string> &bullets)
+{
    bullets.clear();
-   if (!mBody) {
+   if (!mBody)
+   {
       LOG(WARNING) << "Alien attempted to GetShot but is not properly initialized";
       return;
    }
 
-   if (zsocket_poll(mBody, timeout)) {
-      zmsg_t* msg = zmsg_recv(mBody);
-      if (msg && zmsg_size(msg) >= 2) {
-         zframe_t* data = zmsg_pop(msg);
-         if (data) {
-            //remove the first frame
+   if (zsocket_poll(mBody, timeout))
+   {
+      zmsg_t *msg = zmsg_recv(mBody);
+      if (msg && zmsg_size(msg) >= 2)
+      {
+         zframe_t *data = zmsg_pop(msg);
+         if (data)
+         {
+            // remove the first frame
             zframe_destroy(&data);
          }
          int msgSize = zmsg_size(msg);
-         for (int i = 0; i < msgSize; i++) {
+         for (int i = 0; i < msgSize; i++)
+         {
             data = zmsg_pop(msg);
-            if (data) {
+            if (data)
+            {
                std::string bullet;
-               bullet.assign(reinterpret_cast<char*> (zframe_data(data)), zframe_size(data));
+               bullet.assign(reinterpret_cast<char *>(zframe_data(data)), zframe_size(data));
                bullets.push_back(bullet);
                zframe_destroy(&data);
             }
          }
-      } else {
-         if (msg) {
+      }
+      else
+      {
+         if (msg)
+         {
             LOG(WARNING) << "Got Invalid bullet of size: " << zmsg_size(msg);
          }
       }
-      if (msg) {
+      if (msg)
+      {
          zmsg_destroy(&msg);
       }
    }
-
 }
 
 /**
  * Destroy the body and context of the alien.
  */
-Alien::~Alien() {
+Alien::~Alien()
+{
    zsocket_destroy(mCtx, mBody);
    zctx_destroy(&mCtx);
 }
